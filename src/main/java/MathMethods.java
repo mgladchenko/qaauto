6f52c999dca4dc9477627f37bad84a7ce45655a5
Mykola Gladchenko <mykola.gladchenko@gmail.com>
2017-05-16 21:15:50 +0300

    First commit

---
+++ src/main/java/MathMethods.java
@@ -0,0 +1,10 @@
+/**
+ * Created by mykola on 5/13/17.
+ */
+public class MathMethods {
+
+	public static int CalculateSum(int a, int b) {
+		int sum = a+b;
+		return sum;
+	}
+}
---
+++ src/test/java/MathMethodsTest.java
@@ -0,0 +1,12 @@
+/**
+ * Created by mykola on 5/13/17.
+ */
+public class MathMethodsTest {
+
+	public static void main(String[] args) {
+		x = MathMethods.CalculateSum(2,2);
+
+		System.out.println("Hello World!");
+	}
+
+}
